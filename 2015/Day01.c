e81c235febe736f6e2357b691f8e44eb77e90742
0x526f62 <robert_orton@hotmail.com>
2024-12-23 20:50:28 +1030

    Progress is difficult.  Visual Studio is too fucking complicated... But I will persist.

--- 2015/Day01.c
+++ 2015/Day01.c
@@ -1,4 +1,25 @@
-# DAY01 part 1
+// # DAY01 part 1
+// includes the standard input/output library
+#include <stdio.h>
+
+// Day01 will take a file called Day01Input.txt and display a count of the  number of characters.
+
+int main( int argc, char const *argv[] ) {
+	// sanity check the command arguments
+	if ( argc != 2 ) {
+		printf( "Usage: Day01 <filename>\n" );
+		return 1;
+	}
+	
 
 
-int main (arg v, )
+	
+	
+	
+
+
+
+
+
+	return 0;
+}
